29bc5a37770dc324d8213e1ce1567f0d32bc943f
Dominik <derbsland@gmail.com>
2020-11-10 14:25:59 +0100

    Rename unit test class. Add comments.

--- app/src/androidTest/java/ch/zkb/registrierung/ui/registration/RegistrationViewModelTest.kt
+++
@@ -1,7 +0,0 @@
-package ch.zkb.registrierung.ui.registration
-
-class RegistrationViewModelTest {
-
-    // Here could be integration tests of the view model
-
-}
---
+++ app/src/test/java/ch/zkb/registrierung/SomeUnitTest.kt
@@ -0,0 +1,16 @@
+package ch.zkb.registrierung
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+/**
+ * If I had more time I would write unit tests here.
+ */
+class SomeUnitTest {
+
+    @Test
+    fun someTest() {
+        assertEquals(4, 2 + 2)
+    }
+}
